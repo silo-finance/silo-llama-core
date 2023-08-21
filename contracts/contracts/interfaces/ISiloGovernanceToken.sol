d9b8452e4189b9b0bbd5c21c7fac10de56da6680
fcruxen <fcruxen@gmail.com>
2023-08-21 15:40:23 -0300

    Delete contracts/contracts directory

--- contracts/contracts/interfaces/ISiloGovernanceToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-interface ISiloGovernanceToken {
-    function mint(address, uint256) external;
-    function burn(address, uint256) external;
-    function balanceOf(address) external view returns (uint256);
-    function owner() external view returns (address);
-}
