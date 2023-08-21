e4a8196613a11827a950a4b4876503503413615f
fcruxen <fcruxen@gmail.com>
2023-08-21 15:33:11 -0300

    Delete contracts/contracts directory

--- contracts/contracts/external/aave/interfaces/IStakedTokenWithConfig.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.8.13;
-
-import {IStakedToken} from "./IStakedToken.sol";
-
-interface IStakedTokenWithConfig is IStakedToken {
-    function STAKED_TOKEN() external view returns(address); // solhint-disable-line func-name-mixedcase
-}
